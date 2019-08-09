3fdb917bdf4dd1a9e4301785cc14b5ccc866d74f
Eugene Dronov <dronov-ep@yandex.ru>
2019-08-09 17:45:47 +0300

    FirstGet

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -1,21 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { cars } from '../cars';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+// import { Observable, of } from 'rxjs';
+// import { share } from 'rxjs/operators';
 
 @Component({
     selector: 'app-cars-list',
     templateUrl: './cars-list.component.html',
     styleUrls: ['./cars-list.component.css']
 })
+
+@Injectable()
 export class CarsListComponent implements OnInit {
-    cars = cars;
+    dataObs;
+    
 
+    constructor(private http: HttpClient) {
 
-    constructor() {
-        // this.cars = cars;
     }
 
     ngOnInit() {
-
+	this.dataObs = this.http.get('/serve/?func=carslist');
     }
+
+    // onGetCars(data) {
+    // 	this.data = data;
+    // 	console.log(data['carslist']);
+    // }
 
 }
